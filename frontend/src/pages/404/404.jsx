f7dd5fb45b0a0323642f5abfbceb5984a4c2c50d
EgorBabin <babinegor4@gmail.com>
2025-07-28 14:06:22 +0300

    2025.07.26

--- frontend/src/pages/404/404.jsx
+++ frontend/src/pages/404/404.jsx
@@ -1,13 +1,15 @@
 import { Link } from 'react-router-dom'
 import { useTitle } from '@/hooks/useTitle'
+import { Frown } from 'lucide-react';
 // import styles from './404.module.css'
 
 export default function NotFound() {
     useTitle('Не найдено - 404')
     return (
         <>
-            <h1>404 - не найдено</h1>
-            <h2>Вернуться на <Link to="/">Главная</Link></h2>
+            <h1>404</h1>
+            <h2>Мы не нашли страничку по вашему запросу <Frown /></h2>
+            <h3>Вернуться на <Link to="/">Главная</Link></h3>
         </>
     )
 }
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,8 @@
 import { useTitle } from '@/hooks/useTitle'
 
 export default function Home() {
-    useTitle('Дом')
+    const Title = import.meta.env.VITE_NAME;
+    useTitle(Title)
     return (
         <>
             <h1>🏠 Главная страница</h1>
